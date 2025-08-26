0671d4f60ae99a1449adcfc33f64995a23ed47c6
Dmitriy <aua008@gmail.com>
2025-08-26 22:55:11 +0300

    added modals, dropdowns, new warning components and controls, with functionality for opening dropdowns

---
+++ assets/js/dropdown.js
@@ -0,0 +1,92 @@
+const dropdown = {
+	init() {
+		this.bootstrap()
+		this.documentListener()
+	},
+	bootstrap() {
+		this.dropdownBtns = $('.dropdown-btn')
+
+		this.dropdownBtns.on('click', e => {
+			e.stopPropagation()
+
+			const $btn = $(e.currentTarget)
+			const $dropdownWrapper = $btn.closest('.dropdown-wrapper')
+
+			this.closeSiblingDropdowns($dropdownWrapper)
+
+			this.toggleDropdown($dropdownWrapper)
+		})
+	},
+
+	documentListener() {
+		$(document).on('click', e => {
+			const $target = $(e.target)
+			const $closestDropdown = $target.closest('.dropdown-wrapper')
+
+			if (!$closestDropdown.length) {
+				this.closeAllDropdowns()
+			}
+		})
+	},
+
+	closeSiblingDropdowns($currentDropdown) {
+		const $parent = $currentDropdown.parent()
+		const $siblings = $parent.find('> .dropdown-wrapper')
+
+		$siblings.each((index, sibling) => {
+			const $sibling = $(sibling)
+			if (!$sibling.is($currentDropdown)) {
+				this.closeDropdownWithNested($sibling)
+			}
+		})
+	},
+
+	closeAllDropdowns() {
+		$('.dropdown-body').addClass('hidden')
+	},
+
+	closeDropdown($dropdownWrapper) {
+		const $dropdown = $dropdownWrapper || $('.dropdown-wrapper')
+		$dropdown.find('.dropdown-body').first().addClass('hidden')
+	},
+
+	closeDropdownWithNested($dropdownWrapper) {
+		const $dropdown = $dropdownWrapper || $('.dropdown-wrapper')
+
+		$dropdown.find('.dropdown-body').first().addClass('hidden')
+
+		$dropdown.find('.dropdown-body').addClass('hidden')
+	},
+
+	toggleDropdown($dropdownWrapper) {
+		const $dropdown = $dropdownWrapper || $('.dropdown-wrapper')
+		const $dropdownBody = $dropdown.find('.dropdown-body').first()
+		const isHidden = $dropdownBody.hasClass('hidden')
+
+		if (isHidden) {
+			$dropdownBody.removeClass('hidden')
+		} else {
+			this.closeDropdownWithNested($dropdown)
+		}
+	},
+
+	closeByCloseBtn() {
+		$(document).on('click', '.dropdown-body .btn', e => {
+			const $btn = $(e.currentTarget)
+			const $dropdownBody = $btn.closest('.dropdown-body')
+
+			if (
+				$btn.find('svg use[href="#icon-close"]').length ||
+				$btn.hasClass('close-btn')
+			) {
+				e.stopPropagation()
+				$dropdownBody.addClass('hidden')
+			}
+		})
+	},
+}
+
+$(document).ready(function () {
+	dropdown.init()
+	dropdown.closeByCloseBtn()
+})
